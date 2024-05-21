6b6caaf9ca1b5a526ef1188a53e26360c36dbd6c
MD Waliullah <waliullah9099@gmail.com>
2024-05-21 09:10:29 +0600

    explore update and delete

--- src/app/api/demo/route.js
+++ src/app/api/demo/route.js
@@ -3,29 +3,15 @@ import { PrismaClient } from "@prisma/client";
 
 export async function POST(req, res) {
   try {
+    const { searchParams } = new URL(req.url);
+    const id = parseInt(searchParams.get("id"));
+
     const reqBody = await req.json();
 
     const prisma = new PrismaClient();
 
-    let result = await prisma.user.create({
-      data: {
-        email: "ms.habiba@gmail.com",
-        password: "tmrIccha",
-        profile: {
-          create: {
-            firstName: "MS ",
-            lastName: "Habiba ",
-            city: "Jetuya",
-            postCode: "9420",
-          },
-        },
-        post: {
-          create: {
-            title: "Demo",
-            description: "demo",
-          },
-        },
-      },
+    const result = await prisma.employee.delete({
+      where: { id, id },
     });
 
     return NextResponse.json({ status: "success", data: result });
@@ -33,3 +19,24 @@ export async function POST(req, res) {
     return NextResponse.json({ status: "Fail", data: e });
   }
 }
+
+// let result = await prisma.user.create({
+//   data: {
+//     email: "ms.habiba@gmail.com",
+//     password: "tmrIccha",
+//     profile: {
+//       create: {
+//         firstName: "MS ",
+//         lastName: "Habiba ",
+//         city: "Jetuya",
+//         postCode: "9420",
+//       },
+//     },
+//     post: {
+//       create: {
+//         title: "Demo",
+//         description: "demo",
+//       },
+//     },
+//   },
+// });
